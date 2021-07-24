e6501b7cf7c4932bccec9bdc4973f5282b79f98d
Aditya Mohan <adityamhn@Adityas-MacBook-Air.local>
2021-07-24 15:38:08 +0530

    added texture to box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import * as THREE from 'three'
-import { Canvas, useFrame, extend, useThree } from 'react-three-fiber'
-import { useRef } from 'react'
+import { Canvas, useFrame, extend, useThree, useLoader } from 'react-three-fiber'
+import { useRef,Suspense } from 'react'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 extend({ OrbitControls })
@@ -17,16 +17,16 @@ function App() {
 
   const Box = (props) => {
     const ref = useRef()
-
+    const texture = useLoader(THREE.TextureLoader,'https://images.pexels.com/photos/129733/pexels-photo-129733.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')
     useFrame(state => {
-      // ref.current.rotation.x += 0.01
+      ref.current.rotation.x += 0.01
       ref.current.rotation.y += 0.01
     })
     return (
-      <mesh ref={ref} {...props} castShadow receiveShadow>
+      <mesh ref={ref} {...props} castShadow>
         <boxBufferGeometry />
-        <meshPhysicalMaterial color="blue"
-        opacity={1} transparent />
+        <meshPhysicalMaterial map={texture}
+           />
       </mesh>
     )
   }
@@ -56,13 +56,16 @@ function App() {
     <div style={{ height: "100vh", widht: "100vw" }}>
 
       <Canvas
-        shadowMap style={{ background: "black" }} camera={{ position: [1,5,1] }}>
-          <fog attach="fog" args={['white',1,10]} />
+        shadowMap style={{ background: "black" }} camera={{ position: [3,3, 3] }}>
+        <fog attach="fog" args={['white', 1, 10]} />
         <ambientLight intensity={0.2} />
         <Bulb position={[0, 3, 0]} />
         <axesHelper args={[5]} />
         <Orbit />
+        <Suspense fallback={null}>
+
         <Box position={[0, 1, 0]} />
+        </Suspense>
         <Floor position={[0, -0.5, 0]} />
       </Canvas>
     </div>
